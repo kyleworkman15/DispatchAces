b471620de0eecfde6f3ec53506a66c59c23473ce
kevinbarbian14 <kevinbarbian14@augustana.edu>
2018-04-10 21:44:57 -0500

    Made it so you can remove users from the displayed list. Also fixed so that it updates in real time.

--- app/src/main/java/com/example/kevinbarbian14/dispatchaces/MainActivity.java
+++ app/src/main/java/com/example/kevinbarbian14/dispatchaces/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kevinbarbian14.dispatchaces;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
+import android.provider.ContactsContract;
 import android.renderscript.Sampler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,29 +20,44 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends Activity {
     private ListView list;
     private DatabaseReference mDatabase;
+    private DatabaseReference mDatabase2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         list = findViewById(R.id.list);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+        mDatabase = FirebaseDatabase.getInstance().getReference("USERS");
+        mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 ArrayList<String> temp = new ArrayList<String>();
                 for (DataSnapshot shot: dataSnapshot.getChildren()){
-                    long x = shot.getChildrenCount();
                     //temp.add((String)shot.getValue());
 
-                    Log.d("MSG",(String)shot.getKey() + (String)shot.getValue());
-                    temp.add((String)shot.getValue());
+                    Log.d("MSG",(String)shot.getKey());
+                    HashMap<String,String> h = (HashMap<String,String>) shot.getValue();
+                    String info = h.get("Email") + "\n" + h.get("From") + "\n" + h.get("To");
+
+                    temp.add(info);
                 }
                 ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,temp);
+                arrayAdapter.notifyDataSetChanged();
                 list.setAdapter(arrayAdapter);
+                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String info = (String)list.getItemAtPosition(position);
+                        String email = ((String) list.getItemAtPosition(position)).substring(0,info.indexOf("\n"));
+                        DatabaseReference tempRef = FirebaseDatabase.getInstance().getReference().child("USERS").child(email);
+                        tempRef.setValue(null);
+                        Log.d("MSG",email);
+                    }
+                });
             }
 
             @Override
